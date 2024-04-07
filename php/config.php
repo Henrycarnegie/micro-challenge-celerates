94ede709448a3e9eb37f9b5900e37a581919c2a9
Henry Carnegie <henrycarnegie23@gmail.com>
2024-04-08 00:27:47 +0700

    stats

--- php/config.php
+++ php/config.php
@@ -3,13 +3,11 @@
     $username = "root";
     $password = "";
     $database = "micro_challenge_celerates";
-
-    // $conn = mysqli_connect("localhost", "root", "", "micro_challenge_celerates"); 
+    
     $conn = mysqli_connect($host, $username, $password, $database);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    
 ?>
